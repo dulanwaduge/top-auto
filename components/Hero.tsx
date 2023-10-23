ffec5e9e0e06fceee45cdd8a997fcfa73172b839
Dulan Waduge <dulanwaduge@192-168-1-179.tpgi.com.au>
2023-10-23 19:35:38 +1100

    added hero img and created nav/footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,29 +1,42 @@
 "use client";
 
-import Image from 'next/image';
-import {CustomButton} from '@/components'
+import Image from "next/image";
+import { CustomButton } from "@/components";
 
 const Hero = () => {
-  const handleScroll = () => {
-
-  }
+  const handleScroll = () => {};
 
   return (
-    <div className='hero'>
-        <div className='flex-1 pt-36 padding-x'>
-            <h1 className='hero__title'>
-                Find your next car -- quickly and whatever nice line
-            </h1>
-            <p className='hero__subtitle'>Streamline your car shopping experience whatever whatever nice catchy line</p>
-            
-            <CustomButton
-                title="Explore Cars"
-                containerStyles="bg-primary-blue text-white rounded-full mt-10"
-                handleClick={handleScroll}
-            />    
-        </div>
-    </div>
-  )
-}
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find your next car -- quickly and whatever nice line
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car shopping experience whatever whatever nice catchy
+          line
+        </p>
 
-export default Hero
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image
+              src="/hero_2.png"
+              alt="hero"
+              fill
+              quality={100}
+              className="object-contain"
+            ></Image>
+            <div className="hero__image-overlay" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
