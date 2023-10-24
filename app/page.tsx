2e3555c75455a90b7f9c489078302f7ea89c1de9
Dulan Waduge <dulanwaduge@192-168-1-179.tpgi.com.au>
2023-10-24 21:20:57 +1100

    created vehicle catalogue with working search make feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import Image from 'next/image'
 import { Hero } from '@/components'
+import {CustomFilter} from '@/components'
+import {SearchBar} from '@/components'
 
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Hero></Hero>
+      <Hero/>
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className="home__text-container">
+          <h1 className='text-4xl font-extrabold'>Vehicle Catalogue</h1>
+          <p>Explore cars in Sri Lanka</p>
+        </div>
+        <div className='home__filters'>
+          <SearchBar />
+          <div className='home__filter-container'>
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
